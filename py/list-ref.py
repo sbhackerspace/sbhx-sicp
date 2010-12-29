3bc04a649582db9368e1659c44f03399022d3ae5
Steve Phillips <elimisteve@gmail.com>
2010-12-29 02:18:10 -0800

    Wrote length and list-ref functions in Clojure and Python

---
+++ py/list-ref.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# Steve Phillips / elimisteve
+# 2010.12.28
+
+# From http://mitpress.mit.edu/sicp/full-text/book/book-Z-H-15.html
+
+### Original Lisp
+# # (define (list-ref items n)
+# #   (if (= n 0)
+# #       (car items)
+# #       (list-ref (cdr items) (- n 1))))
+
+def list_ref(items, n):
+    if n == 0:
+        return items[0]
+    else:
+        return list_ref(items[1:], n-1)
+
+
+### Original Lisp
+# # (define squares (list 1 4 9 16 25))
+# # (list-ref squares 3)
+# # 16
+
+squares = [1, 4, 9, 16, 25]
+#squares = [x**2 for x in [1, 2, 3, 4, 5] ]
+#squares = [x**2 for x in range(1, 6)]
+print list_ref(squares, 3)  # 16
